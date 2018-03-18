10aa8e2f088bf2f1b8893b5d73b4a526dda5730d
dj-death <dj-death@windowslive.com>
2018-03-18 23:54:06 +0000

    'dd'

--- server.js
+++ server.js
@@ -301,12 +301,7 @@ var gcm = require('node-gcm');
 
 // Replace these with your own values.
 var apiKey = "AAAAO21PC5I:APA91bH-2U7kzg8UnphSxf86uV0vC6fg4b_7VfTGmElO0njyhs25DNk_vvCqOiRheSEjoExBnLMH5IuH5bxw-HorM0ktvzwsGlzukfM6baqktEg5Nya-6qetsh6xv5yfbC6qjuTi1rcS";
-var deviceID = "Device's Registration ID";
 
-service.send(message, { registrationTokens: [ deviceID ] }, function (err, response) {
-	if(err) console.error(err);
-	else 	console.log(response);
-});
 
 var options = {
     keyFile  : 'domain.key',
